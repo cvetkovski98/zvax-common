005271efb04068dfcb6b571f6479f30f42aac614
Oliver Cvetkovski <oliver.cvetkovski@thethingsindustries.com>
2023-01-04 18:45:18 +0100

    Make generate script use local protoc

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
-docker compose run protoc \
-    -I=/proto \
-    --go_out=/gen \
+protoc \
+    -I=./ \
+    --go_out=./ \
     --go_opt=module=github.com/cvetkovski98/zvax-common \
-    --go-grpc_out=. \
+    --go-grpc_out=./ \
     --go-grpc_opt=module=github.com/cvetkovski98/zvax-common \
-    /proto/*/*.proto
+    ./proto/*/*.proto
